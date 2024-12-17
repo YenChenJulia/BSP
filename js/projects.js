632bde6f46df23e860add619b5b46ead29d59fed
Julia <fairy750122@gmail.com>
2024-12-17 09:43:31 +0800

    make a function called createCalculator

--- js/projects.js
+++ js/projects.js
@@ -12,167 +12,166 @@ document.querySelectorAll(".single-client").forEach((element) => {
     hideAllDetail();
     targetProject.classList.remove("d-none");
     // targetProject.classList.add("d-block");
-
-    // ------- Emi Calculator --------- //
-    var SelectedAmount,
-      selectedTime = {},
-      RateOfInterestTime,
-      RateOfInterestAmount,
-      SelectedRoi;
-
-    if (typeof wNumb !== "undefined") {
-      var AmountFormat = wNumb({
-        decimals: 0,
-        thousand: ",",
-        prefix: "$",
-      });
-      var AmountFormatNoMerge = wNumb({
-        decimals: 0,
-        thousand: ",",
-      });
-
-      var TimeFormatMonths = wNumb({
-        suffix: " months",
-      });
-    }
-
-    //Slider Elements
-    var mySlider = targetProject.querySelector(".RangeSlider");
-    var mySliderMonth = targetProject.querySelector(".MonthRangeSlider");
-
-    function clickOnPip(sliderName, This) {
-      var value = Number(This.getAttribute("data-value"));
-      sliderName.noUiSlider.set(value);
-    }
-
-    function SetPipsOnSlider(PipsName, sliderName) {
-      for (var i = 0; i < PipsName.length; i++) {
-        PipsName[i].style.cursor = "pointer";
-        PipsName[i].addEventListener("click", function () {
-          clickOnPip(sliderName, this);
-        });
-      }
-    }
-
-    //   Activate Range Sliders
-
-    if (mySlider && mySliderMonth) {
-      noUiSlider.create(mySlider, {
-        start: [170],
-        connect: "lower",
-        range: {
-          min: 20,
-          max: 500,
-        },
-        format: wNumb({
-          decimals: 0,
-        }),
-        pips: {
-          mode: "values",
-          density: 100,
-
-          values: [20, 100, 200, 300, 400, 500],
-          stepped: true,
-          format: wNumb({
-            encoder: function (a) {
-              return a / 1000;
-            },
-            decimals: 0,
-            prefix: "$",
-            suffix: "k",
-          }),
-        },
-      });
-      noUiSlider.create(mySliderMonth, {
-        start: [18],
-        connect: "lower",
-        range: {
-          min: 12,
-          max: 48,
-        },
-        format: wNumb({
-          decimals: 0,
-        }),
-        pips: {
-          mode: "values",
-          density: 100,
-
-          values: [12, 18, 24, 30, 36, 42, 48],
-          stepped: true,
-          format: wNumb({
-            decimals: 0,
-          }),
-        },
-      });
-      //Slider Pips
-      var pips = mySlider.querySelectorAll(".noUi-value");
-      var pipsMonth = mySliderMonth.querySelectorAll(".noUi-value");
-
-      //Slider Input Element
-      var inputMonthFormat = targetProject.querySelector(".SetMonthRange");
-      var inputFormat = targetProject.querySelector(".SetRange");
-
-      SetPipsOnSlider(pips, mySlider);
-      SetPipsOnSlider(pipsMonth, mySliderMonth);
-      mySlider.noUiSlider.on("update", function (values, handle) {
-        SelectedAmount = AmountFormat.from(values[handle]);
-        CalculateAmount();
-      });
-
-      inputFormat.addEventListener("change", function () {
-        mySlider.noUiSlider.set(this.value);
-      });
-      mySliderMonth.noUiSlider.on("update", function (values, handle) {
-        selectedTime = {
-          type: "month",
-          value: TimeFormatMonths.from(values[handle]),
-        };
-
-        CalculateAmount();
-      });
-
-      inputMonthFormat.addEventListener("change", function () {
-        mySliderMonth.noUiSlider.set(this.value);
-      });
-
-    }
-
-    function CalculateAmount() {
-      // 暫時指定年利率 14%
-
-      var TotalRateOfInterest = 14;
-      var monthlyRate = (TotalRateOfInterest * 0.01) / 12;
-
-      if (monthlyRate === 0) {
-        console.error(
-          "TotalRateOfInterest cannot be zero for this calculation."
-        );
-      } else {
-        var factor = Math.pow(1 + monthlyRate, selectedTime.value * 12);
-        var MinimumMonthlyPayment =
-          (SelectedAmount * (factor * monthlyRate)) / (factor - 1);
-      }
-
-      inputFormat.value = AmountFormatNoMerge.to(SelectedAmount);
-
-      if ($(inputMonthFormat).hasClass("noTextMerge")) {
-        inputMonthFormat.value = selectedTime.value;
-      } else {
-        inputMonthFormat.value = selectedTime.value + " " + selectedTime.type;
-      }
-
-      if (selectedTime.type) {
-        $(".loanTermIndicator").text(selectedTime.type.charAt(0));
-      }
-
-      $(".emiAmount").text(
-        AmountFormat.to(MinimumMonthlyPayment * 1000000) + "（VND）"
-      );
-    } // End of Calculator
+    createCalculator(targetProject, 14);
 
     imgContainer.style.marginTop = "20px";
   });
 });
+
+function createCalculator(projectId, lendingRate) {
+  var SelectedAmount,
+    selectedTime = {},
+    RateOfInterestTime,
+    RateOfInterestAmount,
+    SelectedRoi;
+
+  if (typeof wNumb !== "undefined") {
+    var AmountFormat = wNumb({
+      decimals: 0,
+      thousand: ",",
+      prefix: "$",
+    });
+    var AmountFormatNoMerge = wNumb({
+      decimals: 0,
+      thousand: ",",
+    });
+
+    var TimeFormatMonths = wNumb({
+      suffix: " months",
+    });
+  }
+
+  //Slider Elements
+  var mySlider = projectId.querySelector(".RangeSlider");
+  var mySliderMonth = projectId.querySelector(".MonthRangeSlider");
+
+  function clickOnPip(sliderName, This) {
+    var value = Number(This.getAttribute("data-value"));
+    sliderName.noUiSlider.set(value);
+  }
+
+  function SetPipsOnSlider(PipsName, sliderName) {
+    for (var i = 0; i < PipsName.length; i++) {
+      PipsName[i].style.cursor = "pointer";
+      PipsName[i].addEventListener("click", function () {
+        clickOnPip(sliderName, this);
+      });
+    }
+  }
+
+  //   Activate Range Sliders
+
+  if (mySlider && mySliderMonth) {
+    noUiSlider.create(mySlider, {
+      start: [170],
+      connect: "lower",
+      range: {
+        min: 20,
+        max: 500,
+      },
+      format: wNumb({
+        decimals: 0,
+      }),
+      pips: {
+        mode: "values",
+        density: 100,
+
+        values: [20, 100, 200, 300, 400, 500],
+        stepped: true,
+        format: wNumb({
+          encoder: function (a) {
+            return a / 1000;
+          },
+          decimals: 0,
+          prefix: "$",
+          suffix: "k",
+        }),
+      },
+    });
+    noUiSlider.create(mySliderMonth, {
+      start: [18],
+      connect: "lower",
+      range: {
+        min: 12,
+        max: 48,
+      },
+      format: wNumb({
+        decimals: 0,
+      }),
+      pips: {
+        mode: "values",
+        density: 100,
+
+        values: [12, 18, 24, 30, 36, 42, 48],
+        stepped: true,
+        format: wNumb({
+          decimals: 0,
+        }),
+      },
+    });
+    //Slider Pips
+    var pips = mySlider.querySelectorAll(".noUi-value");
+    var pipsMonth = mySliderMonth.querySelectorAll(".noUi-value");
+
+    //Slider Input Element
+    var inputMonthFormat = projectId.querySelector(".SetMonthRange");
+    var inputFormat = projectId.querySelector(".SetRange");
+
+    SetPipsOnSlider(pips, mySlider);
+    SetPipsOnSlider(pipsMonth, mySliderMonth);
+    mySlider.noUiSlider.on("update", function (values, handle) {
+      SelectedAmount = AmountFormat.from(values[handle]);
+      CalculateAmount(lendingRate);
+    });
+
+    inputFormat.addEventListener("change", function () {
+      mySlider.noUiSlider.set(this.value);
+    });
+    mySliderMonth.noUiSlider.on("update", function (values, handle) {
+      selectedTime = {
+        type: "month",
+        value: TimeFormatMonths.from(values[handle]),
+      };
+
+      CalculateAmount(lendingRate);
+    });
+
+    inputMonthFormat.addEventListener("change", function () {
+      mySliderMonth.noUiSlider.set(this.value);
+    });
+  }
+
+  function CalculateAmount(TotalRateOfInterest) {
+    // 暫時指定年利率 14%
+
+    // var TotalRateOfInterest = 14;
+    var monthlyRate = (TotalRateOfInterest * 0.01) / 12;
+
+    if (monthlyRate === 0) {
+      console.error("TotalRateOfInterest cannot be zero for this calculation.");
+    } else {
+      var factor = Math.pow(1 + monthlyRate, selectedTime.value * 12);
+      var MinimumMonthlyPayment =
+        (SelectedAmount * (factor * monthlyRate)) / (factor - 1);
+    }
+
+    inputFormat.value = AmountFormatNoMerge.to(SelectedAmount);
+
+    if ($(inputMonthFormat).hasClass("noTextMerge")) {
+      inputMonthFormat.value = selectedTime.value;
+    } else {
+      inputMonthFormat.value = selectedTime.value + " " + selectedTime.type;
+    }
+
+    if (selectedTime.type) {
+      $(".loanTermIndicator").text(selectedTime.type.charAt(0));
+    }
+
+    $(".emiAmount").text(
+      AmountFormat.to(MinimumMonthlyPayment * 1000000) + "（VND）"
+    );
+  }
+}
 
 function hideAllDetail() {
   const allDetail = document.querySelectorAll(".detail");
